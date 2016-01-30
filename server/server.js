e6fefd3eab76830b024d46cdee85ddb923216d78
Franci Zidar <francizidar@Francis-MacBook-Pro-2.local>
2016-01-30 11:18:02 +0100

    Website added

--- server/server.js
+++ server/server.js
@@ -3,17 +3,19 @@
  */
 
 var express = require('express');
-var app = express(); // returns the instance of express app/server
+var app     = express(); // returns the instance of express app/server
+
+var serveIndex = require('serve-index');
+
+// does the same thing as express.static() but is compatible with serveIndex
+var serveStatic = require('serve-static');
 
 var PORT = process.env.PORT || 3000;
 
 exports.start = function(){
 
-    app.get('/', function(req, res){
-
-        res.send('Hello world');
-
-    });
+    app.use('/', serveStatic('public'));
+    app.use('/', serveIndex('public'));
 
     app.listen(PORT, function(){
 
