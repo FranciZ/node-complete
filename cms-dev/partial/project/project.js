869f4fd0fd5875fc51e6d4630f732b782369a070
Franci Zidar <francizidar@Francis-MacBook-Pro-2.local>
2016-02-02 17:13:32 +0100

    File upload- added thumbnail, progress, select then upload logic

--- cms-dev/partial/project/project.js
+++ cms-dev/partial/project/project.js
@@ -1,17 +1,33 @@
 angular.module('cms').controller('ProjectCtrl',function($scope, $http, projectService, Upload){
 
+    $scope.options = {};
+
+    $scope.selectFile = function(file){
+
+        $scope.options.selectedFile = file;
+
+    };
+
     $scope.upload = function(file){
+
+        console.log(file);
+
+        $scope.options.fileName = file.name;
 
         Upload.upload({
             url: '/api/upload',
             data: {file: file }
         }).then(function (resp) {
             console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+            $scope.options.fileUploaded = file;
         }, function (resp) {
             console.log('Error status: ' + resp.status);
         }, function (evt) {
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+
+            $scope.options.progressPercentage = progressPercentage;
+
         });
 
     };
